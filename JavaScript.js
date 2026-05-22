d6d9bc7311dde9b35f7de7dc544aa477156c1c68
Gustavo-rgb-ux <gustavorodrigues5740015@gmail.com>
2026-05-22 23:50:50 +0000

    e

--- JavaScript.js
+++ JavaScript.js
@@ -10,5 +10,32 @@ const email = document.getElementById("email").value;
 const senha = document.getElementById("senha").value;
 
 // VALIDAR EMAIL
-const regexEmail = /^[]
+const regexEmail = /^[^/s@]+/.[^/s@]+$/;
+
+if (!regexEmail. test(email)) {
+     mensagem.innerHTML = "Email inválido!";
+     return;
+
+}
+
+// VALIDAR SENHA
+// letra + número + carctere especial + 8 caracteres
+
+const regexSenha -
+/^(?-.*[A-Za-z])(?-.*/d)(?-.*[@$!%*#?&]).{8,}$/;
+
+if (!regexSenha.test(senha)) {
+
+    mensagem.innerHTML =
+    "Senha fraca! Use letra, número e caractere especial.";
+    return
+
+}
+
+// JSON
+const dados = {
+
+    nome: nome,
+    email: email
+}
 })
